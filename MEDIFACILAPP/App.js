94aab851fca961fba557e1a547b88f45ec64abb4
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2022-04-01 00:56:56 -0600

    User Account

--- MEDIFACILAPP/App.js
+++ MEDIFACILAPP/App.js
@@ -1,8 +1,8 @@
 import React, {useEffect} from "react"
 import Navigation from "./app/navigation/Navigation"
-import { firebaseApp } from "./app/utils/Firebase"
+import { firebaseApp } from "./app/utils/firebase"
 
 export default function App(){
-  return <Navigation/>
+  return (<Navigation/>)
 }
 
