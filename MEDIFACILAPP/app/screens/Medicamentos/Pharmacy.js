b7c0aaee697c301418ec7833fb6b029ab788c93b
nauyaca <tiburon-nahu@outlook.com>
2022-04-25 18:41:17 -0500

    aparicion de boton

--- MEDIFACILAPP/app/screens/Medicamentos/Pharmacy.js
+++ MEDIFACILAPP/app/screens/Medicamentos/Pharmacy.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {StyleSheet, View, Text} from 'react-native'
-import {Icon} from 'react-native-elements'
+import {Icon, Input} from 'react-native-elements'
+import {firebaseApp} from '../../utils/Firebase'
+import firebase from 'firebase/app'
+
 
 export default function Pharmacy(){
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo) =>{
+            setUser(userInfo)
+        })
+    }, [])
+
     return(
         <View style={styles.viewBody}>
             <Text>Pharmacy</Text>
+            {user && (
             <Icon 
             reverse
             type='material-community'
             name='plus'
             color= '#00a680'
             containerStyle={styles.btnContainer}
-            />
+            />)}
         </View>
     )
 }
