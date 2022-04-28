ffe360fe95915752b7a8a2117536145225c4ccdc
Nauyaca <tiburon-nahu@outlook.com>
2022-04-28 00:00:42 -0500

    avance

--- MEDIFACILAPP/app/components/Pharmacy/AddPharmacyForm.js
+++ MEDIFACILAPP/app/components/Pharmacy/AddPharmacyForm.js
@@ -3,6 +3,9 @@ import {StyleSheet, Text, View, ScrollView, Alert, ImagePickerIOS } from "react-
 import {Avatar, Button, Input, Icon } from "react-native-elements";
 import CountryPicker from 'react-native-country-picker-modal'
 import {map, size} from 'lodash'
+/* import * as Permissions from 'expo-permissions'
+import * as ImagePicker from 'expo-image-picker' */
+
 
 export default function AddPharmacyForm(props){
     const {toastRef, setLoading, navigation} = props
@@ -45,6 +48,16 @@ export default function AddPharmacyForm(props){
 }
 
 function UploadImage({ toastRef, imagesSelected, setImagesSelected }){
+    
+    /* const imageSelect = async() => {
+    const response = await loadImageFromGallery[[4, 3]]
+        if (!response.status){
+            toastRef.current.show("No has seleccionado ninguna imagen", 3000)
+            return
+        }
+        setImagesSelected([...imagesSelected, response.image])
+    }
+ */
     return(
         <ScrollView
         horizontal
@@ -57,6 +70,7 @@ function UploadImage({ toastRef, imagesSelected, setImagesSelected }){
                 name="camera"
                 color="#7a7a7a"
                 containerStyle={styles.containerIcon}
+               /*  onPress={imageSelect} */
                      />   
                 )
             }
@@ -155,17 +169,25 @@ const defaultFormValues = () => {
     }
 }
 
-/* export const loadImageFromGallery = async(array) => {
-    const response= {status: false, image:null }
+    /* export const loadImageFromGallery = async(array) => {
+    const response = { status: false, image:null }
     const resultPermissions = await Permissions.askAsync (Permissions.CAMERA)
     if (resultPermissions.status === "denied"){
         Alert.alert("Debes de darle permiso para acceder a las imagenes del telefono.")
         return response
     }
     const result = await ImagePicker.launchImageLibraryAsync({
-
+            allowsEditing: true,
+            aspect: array
     })
-}  */  
+    if (result.cancelled){
+        return response
+    }
+    response.status = true
+    response.image = result.uri
+    return response
+}   
+ */
 
 const styles = StyleSheet.create({
     viewContainer: {
